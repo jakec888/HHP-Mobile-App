4b92fe07de118ded818ef2cb1bb8d994fe7bba50
jj888 <jaconjcondes@gmail.com>
2019-11-05 18:32:54 -0800

    added native base fonts and cache functions

--- App.tsx
+++ App.tsx
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {AppLoading} from 'expo';
+import {FontAwesome} from '@expo/vector-icons';
+import {Asset} from 'expo-asset';
+import * as Font from 'expo-font';
+import {Image} from 'react-native';
+
 import Root from './Root';
 
 import Home from './screens/Home';
 
-export default function App() {
-  return (
-    <Root>
-      <Home />
-    </Root>
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font =>
+    Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    }),
   );
 }
+
+export default class App extends Component {
+  state = {
+    isReady: false,
+  };
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+      require('./assets/splash.png'),
+    ]);
+
+    const fontAssets = cacheFonts([FontAwesome.font]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({isReady: true})}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <Root>
+        <Home />
+      </Root>
+    );
+  }
+}
+
+// export default function App() {
+//   return (
+//     <Root>
+//       <Home />
+//     </Root>
+//   );
+// }
