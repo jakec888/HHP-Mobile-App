f0b7de6421652b85314f758a1f260d277aa4b2d4
jj888 <jaconjcondes@gmail.com>
2019-11-11 18:32:43 -0800

    boilerplate multi-screen application due to native base bad ui form features

---
+++ screens/PatientAssistanceApplication/PAA2Screen.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text
+} from "native-base";
+
+import {
+  updateEmail,
+  updateFirstName,
+  updateLastName,
+  updateApplicant
+} from "../../redux/actions/ppa.actions";
+
+export class PAA2Screen extends Component {
+  onChangeEmail = email => {
+    this.props.updateEmail(email);
+  };
+
+  onChangeFirstName = first_name => {
+    this.props.updateFirstName(first_name);
+  };
+
+  onChangeLastName = last_name => {
+    this.props.updateLastName(last_name);
+  };
+
+  onChangeApplicant = applicant => {
+    this.props.updateApplicant(applicant);
+  };
+
+  onSubmitUserData = event => {
+    // action prop to submit form
+    this.props.navigation.navigate("Part3");
+  };
+
+  render() {
+    return (
+      <Container>
+        <Content style={styles.container}>
+          <Form>
+            <Item floatingLabel style={styles.input}>
+              <Label style={styles.label}>Part 2</Label>
+              <Input autoCapitalize="none" onChangeText={this.onChangeEmail} />
+            </Item>
+            <Item floatingLabel style={styles.input}>
+              <Label style={styles.label}>First Name</Label>
+              <Input
+                autoCapitalize="none"
+                onChangeText={this.onChangeFirstName}
+              />
+            </Item>
+            <Item floatingLabel style={styles.input}>
+              <Label style={styles.label}>Last Name</Label>
+              <Input
+                autoCapitalize="none"
+                onChangeText={this.onChangeLastName}
+              />
+            </Item>
+            <View style={styles.buttonContainer}>
+              <Button
+                primary
+                full
+                bordered
+                rounded
+                style={styles.button}
+                onPress={this.onSubmitUserData}
+              >
+                <Text style={styles.buttonText}>Go To Part 3</Text>
+              </Button>
+            </View>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "95%"
+  },
+  input: {
+    borderColor: "#1B6FF0"
+  },
+  label: {
+    color: "#1B6FF0"
+  },
+  buttonContainer: {
+    margin: "5%"
+  },
+  button: {
+    borderColor: "#1B6FF0",
+    color: "#1B6FF0",
+    alignSelf: "center",
+    width: "50%"
+  },
+  buttonText: {
+    color: "#1B6FF0"
+  }
+});
+
+const mapStateToProps = state => ({
+  email: state.PPA.email,
+  first_name: state.PPA.first_name,
+  last_name: state.PPA.last_name,
+  applicant: state.PPA.applicant
+});
+
+const mapDispatchToProps = {
+  updateEmail: updateEmail,
+  updateFirstName: updateFirstName,
+  updateLastName: updateLastName,
+  updateApplicant: updateApplicant
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PAA2Screen);
--- screens/PatientAssistanceApplication/PAAStackNavigator.js
+++ screens/PatientAssistanceApplication/PAAStackNavigator.js
@@ -1,28 +1,42 @@
-import React from 'react'
-import { createStackNavigator } from 'react-navigation-stack'
-import { Platform } from 'react-native'
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { Platform } from "react-native";
 
-import { Text } from 'native-base'
+import { Title, Subtitle, View } from "native-base";
 
-import PAAScreen from './PAAScreen'
+import PAA1Screen from "./PAA1Screen";
+import PAA2Screen from "./PAA2Screen";
+import PAA3Screen from "./PAA3Screen";
 
-import NavigationDrawerStructure from '../../navigation/NavigationDrawerStructure'
+import NavigationDrawerStructure from "../../navigation/NavigationDrawerStructure";
 
 const PAAStackNavigator = createStackNavigator({
-  Sent: {
-    screen: PAAScreen,
+  Part1: {
+    screen: PAA1Screen,
     navigationOptions: ({ navigation }) => ({
       headerTitle: (
-        <Text
-          style={{
-            flex: 1,
-            textAlign: 'center',
-            fontWeight: 'bold',
-            color: 'white'
-          }}
-        >
-          Patient Assistance Application
-        </Text>
+        <View>
+          <Title
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Patient Application
+          </Title>
+          <Subtitle
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            (1 of 3)
+          </Subtitle>
+        </View>
       ),
       ...Platform.select({
         ios: {
@@ -31,11 +45,89 @@ const PAAStackNavigator = createStackNavigator({
         android: {}
       }),
       headerStyle: {
-        backgroundColor: '#1B6FF0'
+        backgroundColor: "#1B6FF0"
       },
-      headerTintColor: '#fff'
+      headerTintColor: "#fff"
+    })
+  },
+  Part2: {
+    screen: PAA2Screen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <View>
+          <Title
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Patient Application
+          </Title>
+          <Subtitle
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            (2 of 3)
+          </Subtitle>
+        </View>
+      ),
+      ...Platform.select({
+        ios: {
+          headerLeft: <NavigationDrawerStructure navigationProps={navigation} />
+        },
+        android: {}
+      }),
+      headerStyle: {
+        backgroundColor: "#1B6FF0"
+      },
+      headerTintColor: "#fff"
+    })
+  },
+  Part3: {
+    screen: PAA3Screen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: (
+        <View>
+          <Title
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            Patient Application
+          </Title>
+          <Subtitle
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white"
+            }}
+          >
+            (3 of 3)
+          </Subtitle>
+        </View>
+      ),
+      ...Platform.select({
+        ios: {
+          headerLeft: <NavigationDrawerStructure navigationProps={navigation} />
+        },
+        android: {}
+      }),
+      headerStyle: {
+        backgroundColor: "#1B6FF0"
+      },
+      headerTintColor: "#fff"
     })
   }
-})
+});
 
-export default PAAStackNavigator
+export default PAAStackNavigator;
