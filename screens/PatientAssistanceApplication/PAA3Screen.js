54d0f073f772f5a686138a8b8f942afacade8094
jj888 <jaconjcondes@gmail.com>
2019-11-17 20:16:31 -0800

    dan abramov presentational and container components for form three

--- screens/PatientAssistanceApplication/PAA3Screen.js
+++ screens/PatientAssistanceApplication/PAA3Screen.js
@@ -1,112 +1,38 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {connect} from 'react-redux';
-import {Container, Content, Textarea, Button, Text} from 'native-base';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import {updateReason, submitPPA} from '../../redux/actions/ppa.actions';
+import { updateReason, submitPPA } from '../../redux/actions/ppa.actions'
+
+import PAA3Form from '../../views/PAA3Form'
 
 export class PAA3Screen extends Component {
-  onChangeReason = reason => {
-    this.props.updateReason(reason);
-  };
+    onChangeReason = reason => {
+        this.props.updateReason(reason)
+    }
 
-  onSubmitUserData = event => {
-    this.props.submitPPA();
-    alert('Submitted!');
-  };
+    onSubmitUserData = event => {
+        this.props.submitPPA()
+        alert('Submitted!')
+    }
 
-  render() {
-    return (
-      <Container>
-        <Content style={styles.container}>
-          <Text style={styles.label}>
-            Please provide a brief description of why you need assistance
-          </Text>
-          <Textarea
-            rowSpan={10}
-            bordered
-            onChangeText={this.onChangeReason}
-            value={this.props.reason}
-            placeholder="Your answer"
-            style={styles.input}
-            placeholderTextColor="#1B6FF0"
-          />
-          <View style={styles.buttonsContainer}>
-            <View style={styles.buttonContainer}>
-              <Button
-                primary
-                full
-                bordered
-                rounded
-                style={styles.buttonBack}
-                onPress={() => {
-                  this.props.navigation.navigate('Part2');
-                }}>
-                <Text style={styles.buttonBackText}>Go Back</Text>
-              </Button>
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                primary
-                full
-                bordered
-                rounded
-                style={styles.buttonNextText}
-                onPress={this.onSubmitUserData}>
-                <Text style={styles.buttonText}>Submit</Text>
-              </Button>
-            </View>
-          </View>
-        </Content>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <PAA3Form
+                onChangeReason={this.onChangeReason}
+                onSubmitUserData={this.onSubmitUserData}
+                reason={this.props.reason}
+            />
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-  },
-  input: {
-    borderColor: '#1B6FF0',
-    color: '#1B6FF0',
-    borderWidth: 5,
-    margin: '2%',
-  },
-  label: {
-    color: '#1B6FF0',
-    textAlign: 'center',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-  },
-  buttonContainer: {
-    flex: 1,
-    margin: '5%',
-  },
-  buttonNext: {
-    borderColor: '#1B6FF0',
-    color: '#1B6FF0',
-  },
-  buttonBack: {
-    borderColor: '#ff0000',
-    color: '#ff0000',
-  },
-  buttonNextText: {
-    color: '#1B6FF0',
-  },
-  buttonBackText: {
-    color: '#ff0000',
-  },
-});
-
 const mapStateToProps = state => ({
-  reason: state.PPA.reason,
-});
+    reason: state.PPA.reason,
+})
 
 const mapDispatchToProps = {
-  updateReason: updateReason,
-  submitPPA: submitPPA,
-};
+    updateReason: updateReason,
+    submitPPA: submitPPA,
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(PAA3Screen);
+export default connect(mapStateToProps, mapDispatchToProps)(PAA3Screen)
