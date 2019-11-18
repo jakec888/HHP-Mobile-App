dce492cccf7fee816f032eb419dd4462ac14d932
jj888 <jaconjcondes@gmail.com>
2019-11-17 19:56:38 -0800

    dan abramov presentational and container components for form one

--- screens/PatientAssistanceApplication/PAA1Screen.js
+++ screens/PatientAssistanceApplication/PAA1Screen.js
@@ -1,125 +1,56 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {connect} from 'react-redux';
-import {
-  Container,
-  Content,
-  Form,
-  Item,
-  Input,
-  Label,
-  Button,
-  Text,
-} from 'native-base';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import PAA1Form from '../../views/PAA1Form'
 
 import {
-  updateEmail,
-  updateFirstName,
-  updateLastName,
-} from '../../redux/actions/ppa.actions';
+    updateEmail,
+    updateFirstName,
+    updateLastName,
+} from '../../redux/actions/ppa.actions'
 
 export class PAA1Screen extends Component {
-  onChangeEmail = email => {
-    this.props.updateEmail(email);
-  };
+    onChangeEmail = email => {
+        this.props.updateEmail(email)
+    }
 
-  onChangeFirstName = first_name => {
-    this.props.updateFirstName(first_name);
-  };
+    onChangeFirstName = first_name => {
+        this.props.updateFirstName(first_name)
+    }
 
-  onChangeLastName = last_name => {
-    this.props.updateLastName(last_name);
-  };
+    onChangeLastName = last_name => {
+        this.props.updateLastName(last_name)
+    }
 
-  onSubmitUserData = event => {
-    // action prop to submit form
-    this.props.navigation.navigate('Part2');
-  };
+    onSubmitUserData = event => {
+        this.props.navigation.navigate('Part2')
+    }
 
-  render() {
-    return (
-      <Container>
-        <Content style={styles.container}>
-          <Form>
-            <Item floatingLabel style={styles.input}>
-              <Label style={styles.label}>Email</Label>
-              <Input
-                style={{color: '#1B6FF0'}}
-                autoCapitalize="none"
-                onChangeText={this.onChangeEmail}
-                value={this.props.email}
-              />
-            </Item>
-            <Item floatingLabel style={styles.input}>
-              <Label style={styles.label}>First Name</Label>
-              <Input
-                style={{color: '#1B6FF0'}}
-                autoCapitalize="none"
-                onChangeText={this.onChangeFirstName}
-                value={this.props.first_name}
-              />
-            </Item>
-            <Item floatingLabel style={styles.input}>
-              <Label style={styles.label}>Last Name</Label>
-              <Input
-                style={{color: '#1B6FF0'}}
-                autoCapitalize="none"
-                onChangeText={this.onChangeLastName}
-                value={this.props.last_name}
-              />
-            </Item>
-            <View style={styles.buttonContainer}>
-              <Button
-                primary
-                full
-                bordered
-                rounded
-                style={styles.button}
-                onPress={this.onSubmitUserData}>
-                <Text style={styles.buttonText}>Next</Text>
-              </Button>
-            </View>
-          </Form>
-        </Content>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <PAA1Form
+                onChangeEmail={this.onChangeEmail}
+                onChangeFirstName={this.onChangeFirstName}
+                onChangeLastName={this.onChangeLastName}
+                onSubmitUserData={this.onSubmitUserData}
+                email={this.props.email}
+                first_name={this.props.first_name}
+                last_name={this.props.last_name}
+            />
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: '95%',
-  },
-  input: {
-    borderColor: '#1B6FF0',
-  },
-  label: {
-    color: '#1B6FF0',
-  },
-  buttonContainer: {
-    margin: '5%',
-  },
-  button: {
-    borderColor: '#1B6FF0',
-    color: '#1B6FF0',
-    alignSelf: 'center',
-    width: '50%',
-  },
-  buttonText: {
-    color: '#1B6FF0',
-  },
-});
-
 const mapStateToProps = state => ({
-  email: state.PPA.email,
-  first_name: state.PPA.first_name,
-  last_name: state.PPA.last_name,
-});
+    email: state.PPA.email,
+    first_name: state.PPA.first_name,
+    last_name: state.PPA.last_name,
+})
 
 const mapDispatchToProps = {
-  updateEmail: updateEmail,
-  updateFirstName: updateFirstName,
-  updateLastName: updateLastName,
-};
+    updateEmail: updateEmail,
+    updateFirstName: updateFirstName,
+    updateLastName: updateLastName,
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(PAA1Screen);
+export default connect(mapStateToProps, mapDispatchToProps)(PAA1Screen)
