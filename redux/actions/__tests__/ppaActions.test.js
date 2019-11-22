01889d6c51c4e379188a6900eab35678b79846da
jj888 <jaconjcondes@gmail.com>
2019-11-21 22:48:11 -0800

    added tests

---
+++ redux/actions/__tests__/ppaActions.test.js
@@ -0,0 +1,5 @@
+describe("Actions Test", () => {
+  it("hello", () => {});
+
+  it("world", () => {});
+});
---
+++ redux/reducers/__tests__/ppaReducers.test.js
@@ -0,0 +1,5 @@
+describe("Reducer Tests", () => {
+  it("hello", () => {});
+
+  it("world", () => {});
+});
